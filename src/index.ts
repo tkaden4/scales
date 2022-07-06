0c0e60c91f003e37c0e82942d4a128793c510189
Kaden Thomas <thomas.kaden4@outlook.com>
2022-07-06 13:35:09 -0700

    Update readme

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import { Note, noteIndex, NoteIndex } from "./note";
-import { getScale, harmonicMinorScale, Scale } from "./scale";
+import { getScale, majorScale, Scale } from "./scale";
 
 function printScale(tuning: Note[], scaleDef: Scale, nfrets: number) {
   const scale = getScale(scaleDef.keynote, scaleDef.scaleFormula);
@@ -8,7 +8,7 @@ function printScale(tuning: Note[], scaleDef: Scale, nfrets: number) {
   const keyNote = scaleIndexes[0];
   let strings = [];
   for (const stringNote of tuning) {
-    let string = `${stringNote} `;
+    let string = `${stringNote}  `;
     const stringNoteIndex = noteIndex(stringNote);
     for (let fret = 0; fret <= nfrets; ++fret) {
       const stringIndex = ((stringNoteIndex + fret) % 12) as NoteIndex;
@@ -25,25 +25,22 @@ function printScale(tuning: Note[], scaleDef: Scale, nfrets: number) {
     }
     strings.push(string);
   }
-  console.log(
-    _.range(0, nfrets + 1)
-      .map((x) => " . .")
-      .join("")
-  );
+
+  const length = strings[0].length;
+  const border = _.range(0, length)
+    .map(() => "=")
+    .join("");
+  console.log(border);
   console.log(scaleDef.name);
   console.log("Tuning:", tuning.join(""));
   console.log(
     " ",
     _.range(0, nfrets + 1)
-      .map((n) => ` ${n}${n < 10 ? " " : ""}`)
-      .join("|")
+      .map((n) => `| ${n}${n < 10 ? " " : ""}`)
+      .join("") + "|"
   );
   console.log(strings.reverse().join("\n"));
-  console.log(
-    _.range(0, nfrets + 1)
-      .map((x) => " . .")
-      .join("")
-  );
+  console.log(border);
 }
 
-printScale(["D", "A", "D", "G", "C", "F"], harmonicMinorScale("D"), 12);
+printScale(["D", "A", "D", "G", "C", "F"], majorScale("D"), 24);
